60700000ed184e4891f9d00b75b90076ce807b9a
ZMK112 <1109645606@qq.com>
2020-03-30 23:28:05 +0800

    isnumber增加部分

--- isNumber.cpp
+++ isNumber.cpp
@@ -2,11 +2,28 @@
 #include <vector>
 using namespace std;
 
+string Uint(string s)
+{
+    while (s[0] > '0' && s[0] < '9'){
+        s.substr(1);
+    }
+    return s; 
+}
+
+string ints(string s)
+{
+    if (s[0] == '+' || s[0] == '-'){
+        s.substr(1);
+    }
+    return Uint(s);
+}
+
 bool isNumber(string s)
 {
     if (s.empty())
     {
         return false;
     }
+    string sOne = 
     
 }
