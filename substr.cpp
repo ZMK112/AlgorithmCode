45f8275358926dd0741892b5db44539b6361ae67
ZMK112 <1109645606@qq.com>
2020-04-01 20:50:25 +0800

    isNumber修改之后提交

---
+++ substr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+　　string as = "12345asdf";
+　　string a = as.substr(0,5);     //获得字符串s中从第0位开始的长度为5的字符串
+    return 0;
+}
