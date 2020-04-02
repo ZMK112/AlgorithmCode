b95149e9f297068a4c8ea130f3155f10d791f2bb
ZMK112 <1109645606@qq.com>
2020-04-02 23:23:57 +0800

    增加栈方式实现的对称二叉树代码

--- isSymmetric.cpp
+++ isSymmetric.cpp
@@ -30,10 +30,36 @@ struct TreeNode {
     return symmetric(a->left, b->right) && symmetric(a->right, b->left);
 } */
 
-bool symmetric(TreeNode* a, TreeNode* b)
+bool isSymmetric(TreeNode* a, TreeNode* b)
 {
-    stack<TreeNode*> s;
-    s.push()
+    stack<TreeNode*> s1;
+    stack<TreeNode*> s2;
+    s1.push(a);
+    s2.push(b);
+
+    bool res = false;
+
+    while (!s1.empty() && !s2.empty())
+    {
+        TreeNode* one = s1.top();
+        s1.pop();
+        TreeNode* two = s2.top();
+        s2.pop();
+
+        if (one->val != two->val)
+        {
+            res = false;
+            return res;
+        }
+
+        s1.push(a->left);
+        s1.push(a->right);
+        s2.push(b->right);
+        s2.push(b->left);
+    }
+
+    res = s1.empty() && s2.empty();
+    return res;
 }
 
 bool isSymmetric(TreeNode* root) 
